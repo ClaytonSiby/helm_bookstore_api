e22f937c2639f55f26a53168e375077bb3520c96
Clayton Siby <claytonsiby@gmail.com>
2023-10-07 11:03:56 +0200

    Add: views to GET and POST books to

--- books_api/views.py
+++ books_api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def books_list(format=None):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def books_create(request):
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
