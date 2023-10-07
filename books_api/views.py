1f1f142fc191cfeaa88f8c1f786dfe2182ae4c62
Clayton Siby <claytonsiby@gmail.com>
2023-10-07 11:20:07 +0200

    Add: the rest of the CRUD actions for the book resource and rename the actions

--- books_api/views.py
+++ books_api/views.py
@@ -5,15 +5,51 @@ from .models import Book
 from .serializers import BookSerializer
 
 @api_view(['GET'])
-def books_list(format=None):
+def get_books_list(format=None):
     books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
-def books_create(request):
+def create_book(request):
     serializer = BookSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_book_details(request, pk):
+    try:
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        book_serializer = BookSerializer(book)
+        return Response(book_serializer.data)
+
+    
+@api_view(['PUT'])
+def update_book(request, pk):
+    try:
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    book_serializer = BookSerializer(book, data=request.data)
+    if book_serializer.is_valid():
+        book_serializer.save()
+        return Response(book_serializer.data)
+    return Response(book_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['DELETE'])
+def delete_book(request, pk):
+    try:
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'DELETE':
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
