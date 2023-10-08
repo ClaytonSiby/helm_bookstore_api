4eadb43658c21197a912095ace26dd3af5a8cc57
Clayton Siby <claytonsiby@gmail.com>
2023-10-08 19:31:26 +0200

    Add: the rest of the endpoints tests for the book

--- books_api/tests.py
+++ books_api/tests.py
@@ -24,3 +24,46 @@ class BookListCreateAPITest(APITestCase):
         self.assertEqual(response.data['book']['author'], self.book_data['author'])
         self.assertEqual(response.data['book']['description'], self.book_data['description'])
         self.assertEqual(Book.objects.count(), 1)
+
+class BookDetailAPITest(APITestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Test Category", description="Test Description")
+        self.book = Book.objects.create(
+            title='Exploring the Galazy',
+            author='Clayton Siby',
+            description='The wonders of our Galaxy and beyond.',
+        )
+
+        self.updated_book_data = {
+            'title': 'Steve Jobs',
+            'author': 'Walter Isaacson',
+            'description': 'Steve Jobs, the man who changed everything',
+        }
+
+        self.book.categories.set([self.category])
+        self.url = reverse('book-detail', args=[self.book.id])
+    
+    def test_retrieve_book(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['book']['id'], str(self.book.id))
+        self.assertEqual(response.data['book']['title'], self.book.title)
+        self.assertEqual(response.data['book']['author'], self.book.author)
+        self.assertEqual(response.data['book']['description'], self.book.description)
+        self.assertEqual(response.data['book']['categories'], [self.book.categories.last().id])
+
+    def test_update_book(self):
+        self.updated_book_data['categories'] = [self.category.id]
+        response = self.client.put(self.url, data=self.updated_book_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['message'], 'Success')
+        self.assertEqual(response.data['book']['title'], self.updated_book_data['title'])
+        self.assertEqual(response.data['book']['author'], self.updated_book_data['author'])
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.title, self.updated_book_data['title'])
+        self.assertEqual(self.book.author, self.updated_book_data['author'])
+        
+    def test_delete_book(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, 204)
+        self.assertFalse(Book.objects.filter(id=self.book.id).exists())
--- helm_bookstore/urls.py
+++ helm_bookstore/urls.py
@@ -10,7 +10,7 @@ from books_api.views import (
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/books/', BookListCreateAPI.as_view(), name='book-list-create'),
-    path('api/books/<int:pk>/', BookDetailAPI.as_view(), name='book-detail'),
+    path('api/books/<uuid:pk>/', BookDetailAPI.as_view(), name='book-detail'),
     path('api/categories/', CategoryListCreateAPI.as_view(), name='category-list-create'),
-    path('api/categories/<int:pk>/', CategoryDetailAPI.as_view(), name='category-detail'),
+    path('api/categories/<uuid:pk>/', CategoryDetailAPI.as_view(), name='category-detail'),
 ]
