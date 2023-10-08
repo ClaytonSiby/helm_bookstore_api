72bf2164766631edf65e4a6935882ac750d0c4bc
Clayton Siby <claytonsiby@gmail.com>
2023-10-08 07:59:57 +0200

    Update: docker composer to user .env values

--- helm_bookstore/settings.py
+++ helm_bookstore/settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / '.helm.env')
+load_dotenv(BASE_DIR / '.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
