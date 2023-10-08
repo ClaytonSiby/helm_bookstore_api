f3d9081f65114054c5365634d851f42fd605aa8f
Clayton Siby <claytonsiby@gmail.com>
2023-10-08 10:41:52 +0200

    Add: category model and update the initializers

--- books_api/models.py
+++ books_api/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     description = models.TextField()
+    categories = models.ManyToManyField('Category', related_name='books')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.title} by {self.author}"
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- books_api/serializers.py
+++ books_api/serializers.py
@@ -1,8 +1,23 @@
+import uuid
 from rest_framework import serializers
-from .models import Book
+from django.contrib.auth.models import User
+from .models import Book, Category
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['pk', 'title', 'author', 'description', 'created_at', 'updated_at']
-        read_only_fields = ['pk']
+        fields = ['id', 'title', 'author', 'description', 'created_at', 'updated_at', 'categories']
+        extra_kwargs = {'id': {'read_only': True, 'default': serializers.CreateOnlyDefault(uuid.uuid4)}}
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'books']
+        extra_kwargs = {'id': {'read_only': True, 'default': serializers.CreateOnlyDefault(uuid.uuid4)}}
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'books', 'created_at', 'updated_at']
+        extra_kwargs = {'id': {'read_only': True, 'default': serializers.CreateOnlyDefault(uuid.uuid4)}}
