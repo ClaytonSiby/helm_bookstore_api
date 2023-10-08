fada08ef05f157b27f7c49f9ea533385ee7c2d19
Clayton Siby <claytonsiby@gmail.com>
2023-10-08 20:21:35 +0200

    Update: build file config

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ set -o errexit
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py test
 
-gunicorn -b 0.0.0.0:8000 --access-logfile - --workers 3 --threads 3 --log-level=debug helm_bookstore.wsgi:application
+gunicorn -b 0.0.0.0:8000 helm_bookstore.wsgi:application
 
 exec "$@"
