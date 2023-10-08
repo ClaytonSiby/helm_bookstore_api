4c20eb8e1ce5f81d6cda811e155c4afb94809c9b
Clayton Siby <claytonsiby@gmail.com>
2023-10-08 10:03:30 +0200

    Update: views and url configurations

--- books_api/views.py
+++ books_api/views.py
@@ -8,14 +8,14 @@ from .serializers import BookSerializer
 def get_books_list(format=None):
     books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
-    return Response(serializer.data)
+    return Response({ "message": "Success", "books": serializer.data }, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def create_book(request):
     serializer = BookSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({ "message": "Success", "book": serializer.data }, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
@@ -23,11 +23,11 @@ def get_book_details(request, pk):
     try:
         book = Book.objects.get(pk=pk)
     except Book.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response({ "message": "Book not found 404" }, status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
         book_serializer = BookSerializer(book)
-        return Response(book_serializer.data)
+        return Response({ "message": "Success", "book": book_serializer.data }, status=status.HTTP_200_OK)
 
     
 @api_view(['PUT'])
@@ -40,8 +40,8 @@ def update_book(request, pk):
     book_serializer = BookSerializer(book, data=request.data)
     if book_serializer.is_valid():
         book_serializer.save()
-        return Response(book_serializer.data)
-    return Response(book_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({ "message": "Success", "book": book_serializer.data }, status=status.HTTP_200_OK)
+    return Response({ "message": "Error", "errors": book_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
     
 @api_view(['DELETE'])
 def delete_book(request, pk):
@@ -52,4 +52,4 @@ def delete_book(request, pk):
     
     if request.method == 'DELETE':
         book.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({ "message": "Success" }, status=status.HTTP_204_NO_CONTENT)
--- helm_bookstore/urls.py
+++ helm_bookstore/urls.py
@@ -1,31 +1,17 @@
 """
 URL configuration for helm_bookstore project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from books_api import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('books/', views.get_books_list),
-    path('books/add_book/', views.create_book),
-    path('books/<int:pk>/', views.get_book_details),
-    path('books/<int:pk>/update/', views.update_book),
-    path('books/<int:pk>/delete/', views.delete_book),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns([
+    path('api/admin/', admin.site.urls),
+    path('api/books/', views.get_books_list, name='books'),
+    path('api/books/add_book/', views.create_book, name="add_book"),
+    path('api/books/<int:pk>/', views.get_book_details, name="book_details"),
+    path('api/books/<int:pk>/update/', views.update_book, name="update_book"),
+    path('api/books/<int:pk>/delete/', views.delete_book, name="delete_book"),
+])
