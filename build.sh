9e4856d9d1889d1cdf7e3235ccdb6f96c482a895
Clayton Siby <claytonsiby@gmail.com>
2023-10-08 20:11:46 +0200

    Update: build.sh for gunicorn server configuration

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py test
 
-gunicorn -b 0.0.0.0:8000 --access-logfile - --workers 3 --threads 3 --log-level=debug helm_books.wsgi:application
+gunicorn -b 0.0.0.0:8000 --access-logfile - --workers 3 --threads 3 --log-level=debug helm_bookstore.wsgi:application
 
 exec "$@"
