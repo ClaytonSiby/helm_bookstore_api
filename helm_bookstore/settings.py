e012b5db298031988cff69398d2f83690449d1a9
Clayton Siby <claytonsiby@gmail.com>
2023-10-08 15:19:33 +0200

    Update: configurations for local database

--- helm_bookstore/settings.py
+++ helm_bookstore/settings.py
@@ -67,12 +67,12 @@ if os.getenv('ENVIRONMENT') == 'production':
 else:
     DATABASES = {
         'default': {
-            'ENGINE': os.getenv('DB_ENGINE'),
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT'),
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'helm_bookstore_db',
+            'USER': 'helm_admin',
+            'PASSWORD': 'helm-pass',
+            'HOST': 'localhost',
+            'PORT': '5432',
         }
     }
 
