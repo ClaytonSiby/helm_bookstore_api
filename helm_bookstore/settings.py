709bbfcd636a186141ec4d48b3501416c147b827
Clayton Siby <claytonsiby@gmail.com>
2023-10-09 10:20:18 +0200

    Update: settings to remove debug instructions

--- helm_bookstore/settings.py
+++ helm_bookstore/settings.py
@@ -11,7 +11,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = False
 
 # allowed hosts
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
@@ -78,7 +78,7 @@ else:
             'NAME': 'bookstore_db',
             'USER': 'helm_admin',
             'PASSWORD': 'password',
-            'HOST': 'db',
+            'HOST': 'localhost',
             'PORT': '5432',
         }
     }
