2c27bc941b51438fc7351f0efd48e9db1a023be6
Clayton Siby <claytonsiby@gmail.com>
2023-10-10 10:59:55 +0200

    Update: failing cors error

--- helm_bookstore/settings.py
+++ helm_bookstore/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'helm_bookstore.urls'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://localhost:8080/',
+    'http://localhost:8080',
     'http://127.0.0.1:8080',
     'http://127.0.0.1:3000',
 ]
