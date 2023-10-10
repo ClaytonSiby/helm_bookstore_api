4f406b61d437a2d2ed1b5fbd33faec9f98cc8cfb
Clayton Siby <claytonsiby@gmail.com>
2023-10-10 12:51:06 +0200

    Update: cores configuration

--- helm_bookstore/settings.py
+++ helm_bookstore/settings.py
@@ -48,12 +48,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'helm_bookstore.urls'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://localhost:8080',
-    'http://127.0.0.1:8080',
-    'http://127.0.0.1:3000',
-]
+CORS_ALLOWED_ORIGINS = ['*']
 
 TEMPLATES = [
     {
