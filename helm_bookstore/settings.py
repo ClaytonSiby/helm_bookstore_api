ac8f7a4edf9cd06c9ee3b1c1bf1592eb094b3db2
Clayton Siby <claytonsiby@gmail.com>
2023-10-10 13:29:26 +0200

    Update: cors configuration

--- helm_bookstore/settings.py
+++ helm_bookstore/settings.py
@@ -53,7 +53,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
     'http://127.0.0.1:8080',
     'http://127.0.0.1:3000',
-    'https://bookstore-application.onrender.com/',
+    'https://bookstore-application.onrender.com',
 ]
 
 TEMPLATES = [
