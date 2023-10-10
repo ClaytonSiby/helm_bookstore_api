e32db2b5bed550433bdadee6f11778c5b1e6cbc3
Clayton Siby <claytonsiby@gmail.com>
2023-10-10 08:45:46 +0200

    Add: a file to create a superuser user and update the build.sh to seed the database

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 python manage.py makemigrations
 python manage.py migrate
+python manage.py shell < books_api/seed_superuser.py
 
 gunicorn helm_bookstore.wsgi:application --bind 0.0.0.0:8000
 
