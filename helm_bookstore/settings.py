88c93aba2567730677d2cb250f91c84afcba7ba8
Clayton Siby <claytonsiby@gmail.com>
2023-10-10 10:53:20 +0200

    Add: a few more localhost for allowed origins

--- helm_bookstore/settings.py
+++ helm_bookstore/settings.py
@@ -50,6 +50,9 @@ ROOT_URLCONF = 'helm_bookstore.urls'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'http://localhost:8080/',
+    'http://127.0.0.1:8080',
+    'http://127.0.0.1:3000',
 ]
 
 TEMPLATES = [
